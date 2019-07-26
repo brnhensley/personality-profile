759aa033d6007db076d2a158da173d44097a68c4
Brian Hensley <brnhensley@gmail.com>
2019-07-26 11:50:26 -0700

    trying to make dev environment compile

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import rootReducer from './reducers/index';
+import { AppContainer } from 'react-hot-loader';
+import { HashRouter } from 'react-router-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const render = (Component) => {
+  ReactDOM.render(
+    <HashRouter>
+      <Provider store={store}>
+        <Component />
+      </Provider>
+    </HashRouter>,
+    document.getElementById('react-app-root')
+  );
+};
+
+render(App);
+
+/*eslint-disable */
+if (module.hot) {
+  module.hot.accept('./components/App', () => {
+    render(App);
+  });
+}
+/*eslint-enable */
