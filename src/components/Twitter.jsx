052ad219dbe076b4cf162b38b3a95e387d464dfa
Brian Hensley <brnhensley@gmail.com>
2019-07-26 16:47:26 -0700

    it will take the sample twitter json call and populate the page with just the tweet

--- src/components/Twitter.jsx
+++ src/components/Twitter.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import sample from './../sample.json'
 
 function Twitter() {
+  console.log(sample);
 
   return (
     <div>
       <h2>Twitter comp here makes API call</h2>
+      <ul>
+        {sample.map((tweet) =>
+          <li>{tweet.text}</li>
+        )}
+      </ul>
     </div>
   );
 }
