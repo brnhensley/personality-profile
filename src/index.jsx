13ee1b92b8c453953d7fd593e98b93218443f537
Brian Hensley <brnhensley@gmail.com>
2019-07-26 13:26:40 -0700

    create components needed later

--- src/index.jsx
+++ src/index.jsx
@@ -6,24 +6,3 @@ import * as serviceWorker from './serviceWorker';
 ReactDOM.render(<App />, document.getElementById('root'));
 
 serviceWorker.unregister();
-
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import rootReducer from './reducers/index';
-// import { HashRouter } from 'react-router-dom';
-// import App from './components/App';
-
-// // const store = createStore(rootReducer);
-
-// const render = (Component) => {
-//   ReactDOM.render(
-//     <div>
-//       <HashRouter>
-//         <Component />
-//       </HashRouter>
-//     </div>,
-//     document.getElementById('react-app-root')
-//   );
-// };
-
-// render(App);
