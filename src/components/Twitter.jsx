d8190089aa67f587b47bd240b14d4cf71d7a3a26
Brian Hensley <brnhensley@gmail.com>
2019-07-28 20:52:53 -0700

    add logic to seperate out tweet data from API call

--- src/components/Twitter.jsx
+++ src/components/Twitter.jsx
@@ -3,17 +3,45 @@ import sample from './../sample.json'
 
 function Twitter() {
   console.log(sample);
+  let tweetText = [];
+  let tweetHTML = [];
+  // sample.map((tweet) =>
+  //   <li>{tweet.text}</li>,
+  //   tweetText.push(tweet.text)
+  // );
+
+  // loops through full Twitter JSON lists them and add the tweets to an array
+  sample.forEach(tweet => {
+    tweetHTML.push(<li>{tweet.text}</li>);
+    tweetText.push(tweet.text);
+  });
+
+  // Finds index of https link and cuts it off before creating master tweetText
+  // tweetText.push(tweet.text.slice(0, tweet.text.indexOf('https://')).trim());
+
+  // makes an array of every word tweeted
+  let wordArray = [];
+  tweetText.join(' ').split(' ').forEach(word => {
+    if (!word.startsWith('https')) {
+      wordArray.push(word)
+    }
+  });
+  //this works, but a lot of punctuation makes it in
+  wordArray.sort();
+  tweetText = wordArray.join(' ')
+
+  console.log(wordArray);
+  console.log(tweetText);
 
   return (
     <div>
       <h2>Twitter comp here makes API call</h2>
       <ul>
-        {sample.map((tweet) =>
-          <li>{tweet.text}</li>
-        )}
+        {tweetHTML}
       </ul>
     </div>
   );
+
 }
 
 export default Twitter;
