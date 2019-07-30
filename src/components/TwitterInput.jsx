eba57f55ea129fe08b538d1737aee49b6dad31fb
Brian Hensley <brnhensley@gmail.com>
2019-07-29 18:56:42 -0700

    add twit

--- src/components/TwitterInput.jsx
+++ src/components/TwitterInput.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-// import express from 'express';
-
 
 class TwitterInput extends React.Component {
   render() {
     let twitterHandle = "";
-    
+    var Twit = require('twit')
+
     let options = {
       method: 'GET',
       url: 'https://api.twitter.com/1.1/statuses/user_timeline.json',
@@ -33,6 +32,14 @@ class TwitterInput extends React.Component {
     // });
 
 
+    var T = new Twit({
+      consumer_key: '91qUH40KKYe3Y5CNUhyGMLFXU',
+      consumer_secret: 'ohIPllkoAoThSkursHRZcBj6Bl5DmugFAsfVNr4257sNggxxmQ',
+      access_token: '1151949815573405696-pEGj46R4jXLMSgbIG32gGMvdMkwxVy',
+      access_token_secret: 'rt8Igro7LumUnekUViby0WVWzerjWrxY3h8FLlUuIm6rf',
+    })
+
+
     function twitterCall(event) {
       event.preventDefault();
       console.log(twitterHandle.value);
@@ -45,6 +52,14 @@ class TwitterInput extends React.Component {
         console.log("it got to the second then")
       )
     }
+
+    function twitterData (err, data, response){
+      console.log(data)
+      console.log("twit worked");
+
+    }
+
+    T.get('statuses/user_timeline', options.qs, twitterData);
 
     // _twitterHandle = input.value.trim()
     //   if (!input.value.trim()) {
@@ -73,3 +88,28 @@ class TwitterInput extends React.Component {
 // }
 
 export default TwitterInput;
+
+//
+// Twit has promise support; you can use the callback API,
+// promise API, or both at the same time.
+//
+T.get('account/verify_credentials', { skip_status: true })
+  .catch(function (err) {
+    console.log('caught error', err.stack)
+  })
+  .then(function (result) {
+    // `result` is an Object with keys "data" and "resp".
+    // `data` and `resp` are the same objects as the ones passed
+    // to the callback.
+    // See https://github.com/ttezel/twit#tgetpath-params-callback
+    // for details.
+
+    console.log('data', result.data);
+  })
+
+//
+// get `funny` twitter users
+//
+T.get('users/suggestions/:slug', { slug: 'funny' }, function (err, data, response) {
+  console.log(data)
+})
