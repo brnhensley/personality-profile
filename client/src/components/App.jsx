7a5d5f9ec9d5e0dd13eda23ee1c1a15b1a1169a1
Student <example@example.com>
2019-07-30 12:40:46 -0700

    add express front end. api proxy works.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -7,10 +7,28 @@ import PropTypes from 'prop-types';
 // STATE
 
 class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: "" };
+  }
+
+  callAPI() {
+    fetch("http://localhost:9000/testAPI")
+      .then(res => res.text())
+      .then(res => this.setState({ apiResponse: res }));
+  }
+
+  componentDidMount() {
+    this.callAPI();
+  }
+
   render() {
+
     return (
       <div>
         <Header />
+        <h1 className="App-intro">{this.state.apiResponse}</h1>
         <Home />
         <Footer />
       </div>
