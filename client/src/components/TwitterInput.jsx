81ce5dbcfb3d03c891d5ecda7f46cbf2cf075fab
Student <example@example.com>
2019-07-30 17:08:45 -0700

    add button to get json log response

--- client/src/components/TwitterInput.jsx
+++ client/src/components/TwitterInput.jsx
@@ -12,30 +12,35 @@ class TwitterInput extends React.Component {
 
   callWatsonAPI() {
     fetch("http://localhost:9000/watsonAPI")
-      .then(res => res.text())
-      .then(console.log(this.state.watsonResponse))
+      .then(res => res.json())
       .then(res => this.setState({ watsonResponse: res }))
-      
   }
 
   componentDidMount() {
     this.callWatsonAPI();
   }
 
+  getState = () => console.log(this.state.watsonResponse);
+
+
   render() {
     return (
       <div>
+        <button onClick={this.getState}>GET STATE</button>
+
         {/* <h1>{this.state.twitterHandle}</h1> */}
-        <h5>{this.state.watsonResponse}</h5>
+        <h5>{JSON.stringify(this.state.watsonResponse)}</h5>
         <form onSubmit={this.callWatsonAPI}>
           <input
             type='text'
             id='twitterHandle'
             placeholder='Enter a Twitter handle'
-            // ref={(input) => { this.setState(twitterHandle: input)); }}
+          // ref={(input) => { this.setState(twitterHandle: input)); }}
           />
           <button type='submit'>Search</button>
         </form>
+
+
       </div>
     );
   }
